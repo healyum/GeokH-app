9dc90c8a1d8a574a3b5301a610949dbe24be0146
Raphael <RaF@MacBook-Air-de-Raphael.local>
2015-02-13 20:07:03 +0100

    Chargement des données

---
+++ geokh/www/js/balises.js
@@ -0,0 +1,85 @@
+/* Fichier d'informations sur les balises du jeu
+ *  une balise est composé de : 
+ *      une latitude
+ *      une logitude
+ *      un identifiant vers une question (sous la forme question_x)
+ *      un indice pour aider à trouver la balise
+ */
+var balises = { 
+    "balises" : {
+        "balise_1" : {
+            "latitude" : "50.610983",
+            "longitude" : "3.139500",
+            "question" : "question_1",
+            "indice" : "Dans l’un des 3 arbres"
+        },
+        "balise_2" : {
+            "latitude" : "50.610000",
+            "longitude" : "3.139500",
+            "question" : "question_2",
+            "indice" : "Entre le buisson et le rocher"
+        },
+        "balise_3" : { //pa sure de la localisation
+            "latitude" : "50.609750",
+            "longitude" : "3.143083",
+            "question" : "question_3",
+            "indice" : "Panneau respecter l’écologie, entre le rocher et le buisson"
+        },
+        "balise_4" : {
+            "latitude" : "50.610383",
+            "longitude" : "3.145567",
+            "question" : "question_4",
+            "indice" : "Dans l’arbre"
+        },
+        "balise_5" : {
+            "latitude" : "50.607767",
+            "longitude" : "3.144467",
+            "question" : "question_5",
+            "indice" : "A droite du panneau Accès pompier, derrière le rocher"
+        },
+        "balise_6" : {
+            "latitude" : "50.607883",
+            "longitude" : "3.146417",
+            "question" : "question_6",
+            "indice" : "SN5, dans l’arbre"
+        },
+        "balise_7" : {
+            "latitude" : "50.608567",
+            "longitude" : "3.141083",
+            "question" : "question_7",
+            "indice" : "Respecter les équilibres écologiques, En dessous du rocher"
+        },
+        "balise_8" : {
+            "latitude" : "50.607700",
+            "longitude" : "3.138683",
+            "question" : "question_8",
+            "indice" : "Dans l’arbre"
+        },
+        "balise_9" : {
+            "latitude" : "50.608433",
+            "longitude" : "3.138683",
+            "question" : "question_9",
+            "indice" : "Sous le panneau de polytech"
+        },
+        "balise_10" : {
+            "latitude" : "50.610300",
+            "longitude" : "3.134650",
+            "question" : "question_10",
+            "indice" : "Derrière le panneau télécom Lille"
+        },
+        "balise_11" : {
+            "latitude" : "50.610383",
+            "longitude" : "3.138500",
+            "question" : "question_11",
+            "indice" : "Regroupez-vous autour"
+        }
+        /* Structure vide d'une balise
+        "balise_x" : {
+            "latitude" : "",
+            "longitude" : "",
+            "question" : "question_x",
+            "indice" : ""
+        }
+        */
+    }
+}
--- geokh/www/js/index.js
+++ geokh/www/js/index.js
@@ -1,22 +1,17 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+/**
+ * 
+ * Auteurs : Goblot Pauline et Bauduin Raphael
+ * 
  */
 var app = {
+    score: 0,
+    equipe: "",
+    niveau: 1,
+    parcours: 1,
+    balise_courante : 10,
+    question_courante : "",
+
+
     // Application Constructor
     initialize: function initialize() {
         console.log("initializing.");
@@ -35,7 +30,9 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+        compass.activateLocation();
+        compass.activateOrientation();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,11 +44,13 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
-    */
+    },*/
     showView: function showView(view_id) {
         $(".view").hide();
         $(view_id).show();
+    },
+    rotate: function rotate(_angle){
+        $('#compass_elt').rotate(_angle);
     }
 };
 
@@ -64,16 +63,103 @@ window.onload = function () {
     });
 
     $('#form_connexion').submit( function( event ) {
+        compass.activateLocation();
+        compass.activateOrientation();
+
         app.showView("#compass");
+        // Affichage du score
+        $('#compass .score .valeur span').text(app.score);
+        $('#compass .conseil .valeur').text(balises.balises["balise_"+app.balise_courante].indice)
+        var latitude = balises.balises["balise_"+app.balise_courante].latitude;
+        var longitude = balises.balises["balise_"+app.balise_courante].longitude;
+
+        compass.data.destination = new LatLon(balises.balises["balise_"+app.balise_courante].latitude, balises.balises["balise_"+app.balise_courante].longitude);
+        // initialisation du chrono
+
+
         event.preventDefault();
     });
 
     $('#btn_flash').click(function() {
         app.showView("#question");
+        $('#question .score .valeur span').text(app.score);
+
+        app.question_courante = balises.balises["balise_"+app.balise_courante].question;
+
+        $('#question .difficulte .valeur span').text(questions.questions[app.question_courante].difficulte);
+        $('#question .lib_question .valeur span').text(questions.questions[app.question_courante].question);
+
+        $('#form_question .reponses').html('');
+        if(questions.questions[app.question_courante].type == "QCM"){
+            for(var i=0; i < questions.questions[app.question_courante].propositions.length; i++){
+                $('#form_question .reponses').append( '<input type="checkbox" name="form_reponse[]" id="form_reponse'+(i+1)+'" value="'+(i+1)+'" /><label for="form_reponse'+(i+1)+'">'+questions.questions[app.question_courante].propositions[i]+'</label>' );
+            }
+        }else if(questions.questions[app.question_courante].type == "QCU"){
+            for(var i=0; i < questions.questions[app.question_courante].propositions.length; i++){
+                $('#form_question .reponses').append( '<input type="radio" name="form_reponse" id="form_reponse'+(i+1)+'" value="'+(i+1)+'" /><label for="form_reponse'+(i+1)+'">'+questions.questions[app.question_courante].propositions[i]+'</label>' );
+            }
+        }
     });
 
     $('#form_question').submit( function( event ) {
         app.showView("#reponse");
+        var input_reponses_courante = $('#form_question .reponses input:checked');
+
+        var reponses_courantes = [];
+        for(var i =0; i< input_reponses_courante.length; i++){
+            reponses_courantes.push($(input_reponses_courante[i]).val()*1);
+        }
+
+        var nb_reponses = questions.questions[app.question_courante].reponses.length;
+        var is_correct= true;
+        if(reponses_courantes.length == nb_reponses){
+            var i = 0;
+            while(is_correct && i < nb_reponses){
+                is_correct = reponses_courantes[i] == questions.questions[app.question_courante].reponses[i];
+                i++;
+            }
+        } else {
+            is_correct = false;
+        }
+
+        if(is_correct){
+            app.score += $('#form_pari').val()*1;
+            $("#reponse .errone").hide();
+            $("#reponse .correct").show();
+            $('#reponse .score .bonus span').text($('#form_pari').val());
+        }else {
+            $("#reponse .errone").show();
+            $("#reponse .correct").hide();
+        }
+        $('#reponse .score .valeur span').text(app.score);
+        var retours = questions.questions[app.question_courante].retour;
+
+        $('#reponse .retour .valeur').html("");
+        for(var p in retours){
+            //$('#reponse .retour .valeur')
+            console.log(typeof retours[p]);
+            console.log(retours[p]);
+            if(typeof retours[p] == "string"){
+                $('#reponse .retour .valeur').append(retours[p]);
+            }else {
+                var liste = "<ul>";
+                for(elt in retours[p]){
+                    liste+= "<li>"+retours[p][elt]+"</li>";
+                }
+                liste+= "</ul>";
+                $('#reponse .retour .valeur').append(liste);
+            }
+        }
+
+
+        if(app.balise_courante == (Object.keys(balises.balises).length-1)){
+            $('#btn_entrepreneurs').show();
+            $('#btn_compass').hide();
+        }else {
+            $('#btn_entrepreneurs').hide();
+            $('#btn_compass').show();
+            app.balise_courante++;
+        }
         event.preventDefault();
     });
 
@@ -82,6 +168,22 @@ window.onload = function () {
     });
     $('#btn_entrepreneurs').click(function() {
         app.showView("#entrepreneurs");
+        var entrepreneurs = entrepreneurs.entrepreneurs;
+
+        for(var i in entrepreneurs){
+            var html_minitaures = '<a href="#" onclick="/*fonction showEnt(id_ent)*/return false;">'
+                                    +'<img src="img/user.svg" alt="'+entrepreneurs[i].prenom+''+entrepreneurs[i].nom+'" class="ent_min" />'
+                                    +'</a>';
+            var html_entrepreneur = '<div id="ent_pres"'+i+'>'
+                                    + '<p class="ent_nom">'+entrepreneurs[i].prenom+''+entrepreneurs[i].nom+'</p>'
+                                    + '<div class="ent_desc">'
+                                    +   '<p>';
+            //ici il faut ajouter les questions et les réponses dans html_entrepreneur
+            html_entrepreneur += '</p>'
+                                + '</div>';
+
+            //les appends (le clear avant)
+        }
     });
 
     $('#btn_entrepreneur_mystere').click(function() {
